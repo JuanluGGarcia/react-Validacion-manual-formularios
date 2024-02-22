76fda517114f42fe74cc6542cd9271d1a9e2db54
Juan Luis Garcia Garcia <juanluisggarcia@gmail.com>
2024-02-22 01:00:40 +0100

    validacion casi terminada

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,31 +1,144 @@
+import { useState } from 'react';
+
 const Contact = () => {
+	const [formValues, setFormValues] = useState({
+		name: '',
+		email: ''
+	});
+
+	const [errors, setErrors] = useState({
+		name: false,
+		email: false
+	});
+
+	console.log(formValues);
+
 	return (
 		<form onSubmit={handleSubmit}>
 			<div>
 				<label htmlFor='name'>NAME</label>
-				<input type='text' id='name' name='name' />
+				<input
+					type='text'
+					id='name'
+					name='name'
+					value={formValues.name}
+					/* función de un campo solo */
+					// onChange={event =>
+					// 	changeName(event.target.value, formValues, setFormValues)
+					// }
+
+					/* función de todos los campos */
+					onChange={event =>
+						changeFormValues(
+							event.target,
+							formValues,
+							setFormValues,
+							errors,
+							setErrors
+						)
+					}
+				/>
+				{errors.name && <span>El nombre no es correcto</span>}
 			</div>
 			<div>
 				<label htmlFor='email'>EMAIL</label>
-				<input type='text' id='email' name='email' />
+				<input
+					type='text'
+					id='email'
+					name='email'
+					value={formValues.email}
+					/* función de un campo solo */
+					// onChange={event =>
+					// 	changeEmail(event.target.value, formValues, setFormValues)
+					// }
+
+					/* función de todos los campos */
+					onChange={event =>
+						changeFormValues(
+							event.target,
+							formValues,
+							setFormValues,
+							errors,
+							setErrors
+						)
+					}
+				/>
+				{errors.email && <span>El email no es correcto</span>}
 			</div>
-            <input type='submit' value='SEND' />
-		
-        </form>
+			<input 
+                type='submit' 
+                value='SEND'
+                disabled={!Object.values(errors).every(error => !error)} 
+            />
+		</form>
 	);
 };
 
+const validateForm = (name, value, errors, setErrors) => {
+	const formatedValue = value.trim();
+	// Expresión regular sólo admite letras
+	const regexName = /^[a-z]+$/gi;
+	const regexEmail =
+		/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+
+	if (name === 'name') {
+		// validación
+		const isValidName = !regexName.test(formatedValue);
+
+		console.log(isValidName);
+		setErrors({ ...errors, name: isValidName });
+	}
+
+	if (name === 'email') {
+		// validación
+		const isValidEmail = !regexEmail.test(formatedValue);
+
+		setErrors({ ...errors, email: isValidEmail });
+	}
+};
+
+/* Todos los valores juntos en la misma función */
+const changeFormValues = (
+	input,
+	formValues,
+	setFormValues,
+	errors,
+	setErrors
+) => {
+	// const name = input.name;
+	// const value = input.value;
+
+	// Esto de aqui arriba se puede poner desestructurando:
+
+	const { name, value } = input;
+
+	// console.log(name); me da name o email
+
+	// Se pone name entre corchetes para acceder al valor de la propiedad name, que es name o email
+	setFormValues({ ...formValues, [name]: value });
+	validateForm(name, value, errors, setErrors);
+};
+
+/* Cada valor por separado */
+/* const changeName = (value, formValues, setFormValues) => {
+	setFormValues({ ...formValues, name: value });
+};
+
+const changeEmail = (value, formValues, setFormValues) => {
+	setFormValues({ ...formValues, email: value });
+}; */
+
 const handleSubmit = event => {
-    event.preventDefatult();
-	console.log('NAME', event.target.name.value);
-	console.log('EMAIL', event.target.email.value);
+	event.preventDefault();
+	// console.log('NAME', event.target.name.value);
+	// console.log('EMAIL', event.target.email.value);
 };
 /* 
     FORMS
 
-    NO CONTROLADA -> Los datos del formulario no están controlados por React. Los datos se obtienen a través de un evento del DOM (JScript)
+    NO CONTROLADA -> Los datos del formulario no están controlados por React. Los datos se obtienen a través de un evento del DOM(JScript).
 
-    CONTROLADA
+    CONTROLADA -> Los datos del formulario los controla React. Los datos se obtienen a través de un estado.
 */
 
 export default Contact;
