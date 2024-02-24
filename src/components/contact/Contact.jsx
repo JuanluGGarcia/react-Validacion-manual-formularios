d31f563133ae44003790cd3253447cebcf043702
Juan Luis Garcia Garcia <juanluisggarcia@gmail.com>
2024-02-24 11:58:57 +0100

    ahora si funciona todo

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,59 +1,45 @@
 import { useState } from 'react';
 
 const Contact = () => {
-
-	// Validación usando 2 estados
-	// Estado para guardar los valores del formulario
-	// const [formValues, setFormValues] = useState({
-	// 	name: '',
-	// 	email: ''
-	// });
-
-	// Estado para gestionar los errores del formulario. Sólo sirve para mostrar los mensajes
-	// const [errors, setErrors] = useState({
-	// 	name: false,
-	// 	email: false
-	// });
-
+	
 	// Validación usando 1 sólo estado
 	const [formValues, setFormValues] = useState({
 		name: '',
 		email: '',
+		isDirty: {
+			name: false,
+			email: false
+		},
 		errors: {
 			name: false,
 			email: false
 		}
+		
 	});
 
-	console.log(formValues);
+	// console.log(formValues)
 
 	return (
 		<form onSubmit={handleSubmit}>
+			
 			<div>
 				<label htmlFor='name'>NAME</label>
 				<input
 					type='text'
 					id='name'
 					name='name'
-					value={formValues.name}
-					/* función de un campo solo */
-					// onChange={event =>
-					// 	changeName(event.target.value, formValues, setFormValues)
-					// }
-
-					/* función de todos los campos */
+					// value={formValues.name}
+					
 					onChange={event =>
 						changeFormValues(
 							event.target,
 							formValues,
-							setFormValues,
-							// errors,
-							// setErrors
+							setFormValues
 						)
 					}
 				/>
-				{/* {errors.name && <span>El nombre no es correcto</span>} */}
-				{formValues.errors.name && <span>El nombre no es correcto</span>}
+				
+				{formValues.isDirty.name && formValues.errors.name && <span>El nombre no es correcto</span>}
 			</div>
 			<div>
 				<label htmlFor='email'>EMAIL</label>
@@ -61,44 +47,35 @@ const Contact = () => {
 					type='text'
 					id='email'
 					name='email'
-					value={formValues.email}
-					/* función de un campo solo */
-					// onChange={event =>
-					// 	changeEmail(event.target.value, formValues, setFormValues)
-					// }
+					// value={formValues.email}
 
-					/* función de todos los campos */
 					onChange={event =>
 						changeFormValues(
 							event.target,
 							formValues,
-							setFormValues,
-							// errors,
-							// setErrors
+							setFormValues
 						)
+						
 					}
 				/>
-				{/* {errors.email && <span>El email no es correcto</span>} */}
-				{formValues.errors.email && <span>El email no es correcto</span>}
+				
+				{formValues.isDirty.email && formValues.errors.email && <span>El email no es correcto</span>}
 			</div>
-			<input 
-                type='submit' 
-                value='SEND'
-                // disabled={!Object.values(errors).every(error => !error)} 
-                disabled={!Object.values(formValues.errors).every(error => !error)} 
-            />
+			<input
+				type='submit'
+				value='SEND'
+				disabled={!Object.values(formValues.errors).every(error => !error)}
+			/>
 		</form>
 	);
 };
 
 const validateForm = (
-	name, 
-	value, 
-	// errors, 
-	// setErrors, 
-	formValues, 
+	name,
+	value,
+	formValues,
 	setFormValues
-	) => {
+) => {
 	const formatedValue = value.trim();
 	// Expresión regular sólo admite letras
 	const regexName = /^[a-z]+$/gi;
@@ -106,76 +83,55 @@ const validateForm = (
 		/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
 	if (name === 'name') {
-		
 		const isValidName = !regexName.test(formatedValue);
 
-		// Validando con 2 estados
-		// console.log(isValidName);
-		// setErrors({ ...errors, name: isValidName });
-
-		// Validando con 1 sólo estado
 		setFormValues({
 			...formValues,
-			errors: {
-				...formValues.errors,
-				name: isValidName
+			errors: { 
+				...formValues.errors, 
+				name: isValidName 
+			},
+			isDirty: {
+				...formValues.isDirty,
+				name: true
 			}
-		})
+		});
 	}
 
 	if (name === 'email') {
-		// validación
 		const isValidEmail = !regexEmail.test(formatedValue);
-
-		// setErrors({ ...errors, email: isValidEmail });
 
 		setFormValues({
 			...formValues,
-			errors: {
-				...formValues.errors,
-				email: isValidEmail
+			errors: { 
+				...formValues.errors, 
+				email: isValidEmail 
+			},
+			isDirty: {
+				...formValues.isDirty,
+				email: true
 			}
-		})
+		});
 	}
 };
 
 /* Todos los valores juntos en la misma función */
-const changeFormValues = (
-	input,
-	formValues,
-	setFormValues,
-	// errors,
-	// setErrors
-) => {
-	// const name = input.name;
-	// const value = input.value;
+const changeFormValues = (input, formValues, setFormValues) => {
 
-	// Esto de aqui arriba se puede poner desestructurando:
-
+	// desestructurando:
 	const { name, value } = input;
-
-	// console.log(name); me da name o email
 
 	// Se pone name entre corchetes para acceder al valor de la propiedad name, que es name o email
 	setFormValues({ ...formValues, [name]: value });
-	// validateForm(name, value, errors, setErrors);
 	validateForm(name, value, formValues, setFormValues);
 };
 
-/* Cada valor por separado */
-/* const changeName = (value, formValues, setFormValues) => {
-	setFormValues({ ...formValues, name: value });
-};
-
-const changeEmail = (value, formValues, setFormValues) => {
-	setFormValues({ ...formValues, email: value });
-}; */
-
 const handleSubmit = event => {
 	event.preventDefault();
-	// console.log('NAME', event.target.name.value);
-	// console.log('EMAIL', event.target.email.value);
 };
+
+
+export default Contact;
 /* 
     FORMS
 
@@ -183,5 +139,3 @@ const handleSubmit = event => {
 
     CONTROLADA -> Los datos del formulario los controla React. Los datos se obtienen a través de un estado.
 */
-
-export default Contact;
